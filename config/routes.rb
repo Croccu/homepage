e8c4606bd0ebdd46a110c66fc7f91fa924c8e67f
rico paum <ricsens@me.com>
2023-12-07 17:00:39 +0200

    static pages and model implemented

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
+
+  resources :projects, only: [ :index, :show ]
 end
